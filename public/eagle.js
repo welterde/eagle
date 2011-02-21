3cff4aa02d40c94f823203efce7ee72e9e6a495f
Omachonu Ogali <oogali@roadrunner.lab.idlepattern.com>
2011-02-21 18:22:59 -0500

    some js changes

--- public/eagle.js
+++ public/eagle.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     alert($('input#value'));
     switch($('select#action').val()) {
       case 'prefix':
+        $('input#value').attr('name', 'prefix');
         $('input#value').attr('placeholder', 'Enter an IP address or CIDR prefix (1.2.3.4, 5.6.7.0/24, etc)...');
         break;
       case 'asn':
